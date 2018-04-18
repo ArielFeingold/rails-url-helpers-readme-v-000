7a5f9df4ac6758536cbf6ee93dd740fe54a30353
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 09:49:30 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
-  get 'posts' => 'posts#index'
+  get '/posts' => 'posts#index' 
+  get '/register', to: 'users#new', as: 'register'
 end
